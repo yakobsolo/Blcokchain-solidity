2da545559366636f9dbc1e35512882bfb6c85aec
yakobsolo <yakisolo2009@gmail.com>
2024-05-25 14:16:14 +0300

    fit(created a coin token)

---
+++ MyCoin.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+
+contract MyCoin {
+
+    address minter;
+    mapping (address => uint) balances;
+
+
+    event havSent(address from, address to,uint amount);
+
+    error insufficientBalance( uint senderbalance, uint amountrequired);
+
+    constructor () {
+        minter = msg.sender;
+        
+
+    }
+
+
+    function mint(address receiver, uint amount) public  {
+        require(msg.sender == minter, "sender is not the minter");
+        balances[receiver] += amount;
+
+    }
+
+    function Send(address receiver, uint amount) public {
+
+        if (balances[msg.sender] < amount) {
+            revert insufficientBalance({senderbalance: balances[msg.sender], amountrequired: amount});
+        }
+
+        balances[msg.sender] -=amount;
+        balances[receiver] += amount;
+
+        emit havSent(msg.sender, receiver, amount);
+    }
+
+
+}
